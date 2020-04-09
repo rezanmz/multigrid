3670b02d78901987102164d6d7322af0abc2e56d
reza <rezanmz@ymail.com>
2020-04-09 08:17:53 +0430

    added cmake

---
+++ cmake-build-debug/CMakeFiles/multigrid.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multigrid.dir/CRS.cpp.o"
+  "CMakeFiles/multigrid.dir/MultigridSolver.cpp.o"
+  "CMakeFiles/multigrid.dir/ReadMatrices.cpp.o"
+  "CMakeFiles/multigrid.dir/Smoother.cpp.o"
+  "CMakeFiles/multigrid.dir/main.cpp.o"
+  "multigrid"
+  "multigrid.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multigrid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/multigrid.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for multigrid.
+# This may be replaced when dependencies are built.
